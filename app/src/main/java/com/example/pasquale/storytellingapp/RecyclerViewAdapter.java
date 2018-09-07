eb8d21de07e2fe5c35ddb7ef50efdfb5612eaa68
AlfonsoAquino <alfonso.aquino.1996@gmail.com>
2018-09-07 18:19:17 +0200

    aggiornamento statistiche/backup

--- app/src/main/java/com/example/pasquale/storytellingapp/RecyclerViewAdapter.java
+++ app/src/main/java/com/example/pasquale/storytellingapp/RecyclerViewAdapter.java
@@ -47,7 +47,7 @@ public class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerViewAdapte
 
     @Override
     public void onBindViewHolder(MyViewHolder holder, final int position) {
-        String fileName = "41.jpg";
+        String fileName = String.valueOf(mData.get(position).getId())+".jpg";
         String completePath = Environment.getExternalStorageDirectory()+"/"+Config.destAlbumPrev+"/"+fileName;
 
         File file = new File(completePath);
@@ -60,17 +60,16 @@ public class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerViewAdapte
             holder.cardView.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View v) {
-//                    if(mData.get(position).getTipo()==2) {
-//                        Intent intent = new Intent(mContext, SequenceGameActivity.class);
-//
-//                        // passing data to the album activity
-//                        intent.putExtra("tipo", mData.get(position).getTipo());
-//                        Log.i("ajsoidaosdoaisjdoaisdjo",mData.get(position).getTipo()+"-----------<");
-//                        intent.putParcelableArrayListExtra("arrayVignette", mData.get(position).getVignette());
-//                        // start the activity
-//                        mContext.startActivity(intent);
-//
-//                    }else {
+                    if(mData.get(position).getTipo()==2){
+                        Intent intent = new Intent(mContext, SequenceGameActivity.class);
+
+                        // passing data to the album activity
+                        intent.putExtra("tipo", mData.get(position).getTipo());
+                        Log.i("ajsoidaosdoaisjdoaisdjo", mData.get(position).getTipo() + "-----------<");
+                        intent.putParcelableArrayListExtra("arrayVignette", mData.get(position).getVignette());
+                        // start the activity
+                        mContext.startActivity(intent);
+                    }else {
                         Intent intent = new Intent(mContext, StoryGameActivity.class);
 
                         // passing data to the album activity
@@ -79,7 +78,7 @@ public class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerViewAdapte
                         intent.putParcelableArrayListExtra("arrayVignette", mData.get(position).getVignette());
                         // start the activity
                         mContext.startActivity(intent);
-//                    }
+                    }
                 }
             });
         }
